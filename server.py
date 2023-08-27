bbeeab69c64a66a3ade19e137dbf081f1cd9f91e
Abdul <abdulhadi12235@gmail.com>
2023-08-28 02:04:51 +0530

    first revert

--- server.py
+++ server.py
@@ -1,27 +1,22 @@
-from flask import Flask, request, jsonify
-from dataclasses import dataclass
-
-@dataclass
-class Result:
-    result: int
-
+from flask import Flask
+from flask import  request
 
 app = Flask(__name__)
 
 
 @app.route("/calculator/greeting", methods=['GET'])
 def greeting():
-    return 'Hello world!'
+    return 'Hello World'
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
-    numbers = request.json
+    numbers = request.json 
     response = Result(numbers['first'] + numbers['second'])
     return jsonify(response)
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
-    numbers = request.json
+    numbers = request.json 
     response = Result(numbers['first'] - numbers['second'])
     return jsonify(response)
 
